bb08b71006ad4eb2e55b30de627c4cfba2b81d31
David <david.deutsch306@gmail.com>
2020-11-10 13:51:50 +0100

    Initial Commit

---
+++ Gradients/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gradients
+//
+//  Created by David Deutsch on 10.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
